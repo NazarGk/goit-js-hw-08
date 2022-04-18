e407253b3a7cadc3775665b23908dbf534a6f867
Nazar Gaiduk <child.tmabc@gmail.com>
2022-04-18 12:22:33 +0300

    Task 0nE

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryList = createListOfGallery(galleryItems);
+
+function createListOfGallery(list) {
+    return galleryItems.map(list => `
+    <a class="gallery__item" href="${list.original}">
+  <img class="gallery__image" src="${list.preview}" alt="${list.description}" />
+</a>`).join('');
+}
+
+galleryEl.innerHTML = galleryList;
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, captionPosition: 'bottom'});
+
